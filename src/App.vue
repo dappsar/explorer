58f2626444d09fb9eca5fcda376543b553764dbc
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2019-03-14 16:06:32 +0300

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,238 +1,240 @@
 <template>
-<div id="app">
+  <div id="app">
 
     <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="https://lition.de">
-                    <img class="logo" src="https://www.lition.io/wp-content/uploads/2018/03/lition-logo-secondary-white@3x.png">
-                </a>
-            </div>
-            <div class="navbar-header navbar-right">
-
-            </div>
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="https://lition.de">
+            <img class="logo" src="https://www.lition.io/wp-content/uploads/2018/03/lition-logo-secondary-white@3x.png">
+          </a>
         </div>
+        <div class="navbar-header navbar-right">
 
-        <div class="alert alert-warning alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <div class="message">warning</div>
         </div>
+      </div>
 
-        <div class="alert alert-info alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <div class="message">info</div>
-        </div>
+      <div class="alert alert-warning alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+          aria-hidden="true">&times;</span></button>
+        <div class="message">warning</div>
+      </div>
 
-        <div class="alert alert-danger alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <div class="message">danger</div>
-        </div>
+      <div class="alert alert-info alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+          aria-hidden="true">&times;</span></button>
+        <div class="message">info</div>
+      </div>
+
+      <div class="alert alert-danger alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+          aria-hidden="true">&times;</span></button>
+        <div class="message">danger</div>
+      </div>
     </nav>
 
     <div class="container-fluid header">
-        <div class="row no_overflow">
-            <div class="col-md-6 col-md-push-6">
-                <img class="headerimage" src="https://www.lition.io/wp-content/uploads/2018/03/180327-den-stage-illusration@3x.png">
-            </div>
-            <div class="col-md-6 col-md-pull-6">
-                <div class="container-fluid title-container">
-                    <h1> <strong>Lition Block Explorer for Energy Case</strong></h1>
-                    <br>
-                    <Metamask/>
-                    <Network/>
-                    <div class="container-fluid text-left contact-container">
-                        <Contract/>
-                    </div>
-                </div>
-            </div>
-            <div class="circle_container"><div class="circle"></div></div>
+      <div class="row no_overflow">
+        <div class="col-md-6 col-md-push-6">
+          <img class="headerimage"
+               src="https://www.lition.io/wp-content/uploads/2018/03/180327-den-stage-illusration@3x.png">
         </div>
+        <div class="col-md-6 col-md-pull-6">
+          <div class="container-fluid title-container">
+            <h1><strong>Lition Block Explorer for Energy Case</strong></h1>
+            <br>
+            <Network/>
+            <div class="container-fluid text-left contact-container">
+              <Contract/>
+            </div>
+          </div>
+        </div>
+        <div class="circle_container">
+          <div class="circle"></div>
+        </div>
+      </div>
     </div>
-    
+
     <Transactions/>
 
     <div class="btn-pref btn-group btn-group-justified btn-group-lg" role="group" aria-label="...">
-        <div class="btn-group" role="group">
-            <button type="button" id="stars" class="btn btn-primary" href="#events" data-toggle="tab">
-                <h4>Events</h4>
-            </button>
-        </div>
-        <div class="btn-group" role="group">
-            <button type="button" id="favorites" class="btn btn-default" href="#bids" data-toggle="tab">
-                <h4>Bids</h4>
-            </button>
-        </div>
-        <div class="btn-group" role="group">
-            <button type="button" id="following" class="btn btn-default" href="#asks" data-toggle="tab">
-                <h4>Asks</h4>
-            </button>
-        </div>
+      <div class="btn-group" role="group">
+        <button type="button" id="stars" class="btn btn-primary" href="#events" data-toggle="tab">
+          <h4>Events</h4>
+        </button>
+      </div>
+      <div class="btn-group" role="group">
+        <button type="button" id="favorites" class="btn btn-default" href="#bids" data-toggle="tab">
+          <h4>Bids</h4>
+        </button>
+      </div>
+      <div class="btn-group" role="group">
+        <button type="button" id="following" class="btn btn-default" href="#asks" data-toggle="tab">
+          <h4>Asks</h4>
+        </button>
+      </div>
     </div>
 
     <div class="tab-content">
-        <div id="events" class="tab-pane fade in active">
-            <Events/>
-        </div>
-        <div id="bids" class="tab-pane fade">
-            <Bids/>
-        </div>
-        <div id="asks" class="tab-pane fade">
-            <Asks/>
-        </div>
+      <div id="events" class="tab-pane fade in active">
+        <Events/>
+      </div>
+      <div id="bids" class="tab-pane fade">
+        <Bids/>
+      </div>
+      <div id="asks" class="tab-pane fade">
+        <Asks/>
+      </div>
     </div>
-    
-    
-    
-</div>
+
+
+  </div>
 </template>
 
 <script>
-import Metamask from "./components/Metamask.vue";
-import Network from "./components/Network.vue";
-import Contract from "./components/Contract.vue";
-import Transactions from "./components/Transactions.vue";
-import Events from "./components/Events.vue";
-import Bids from "./components/Bids.vue";
-import Asks from "./components/Asks.vue";
-import sharedState from './state.js'
-import energyStoreState from './EnergyStoreState.js'
+  import Network from "./components/Network.vue";
+  import Contract from "./components/Contract.vue";
+  import Transactions from "./components/Transactions.vue";
+  import Events from "./components/Events.vue";
+  import Bids from "./components/Bids.vue";
+  import Asks from "./components/Asks.vue";
+  import sharedState from './state.js'
+  import energyStoreState from './EnergyStoreState.js'
 
-export default {
+  export default {
     name: "app",
     components: {
-        Metamask,
-        Network,
-        Contract,
-        Transactions,
-        Events,
-        Bids,
-        Asks
+      Network,
+      Contract,
+      Transactions,
+      Events,
+      Bids,
+      Asks
     },
     data() {
-        return sharedState;
+      return sharedState;
     },
     watch: {
-        address: energyStoreState.handleNewAddress,
-        contract: energyStoreState.handleNewContract
+      address: energyStoreState.handleNewAddress,
+      contract: energyStoreState.handleNewContract
     },
     mounted: function () {
-        this.txhash = window.location.search.replace('?tx/', '');
+      this.txhash = window.location.search.replace('?tx/', '');
     }
-};
+  };
 
-$(document).ready(function() {
-$(".btn-pref .btn").click(function () {
-    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-    // $(".tab").addClass("active"); // instead of this do the below 
-    $(this).removeClass("btn-default").addClass("btn-primary");   
-});
-});
+  $(document).ready(function () {
+    $(".btn-pref .btn").click(function () {
+      $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+      // $(".tab").addClass("active"); // instead of this do the below
+      $(this).removeClass("btn-default").addClass("btn-primary");
+    });
+  });
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
 
-
-nav.navbar > .alert {
+  nav.navbar > .alert {
     display: none;
     margin-bottom: 0;
-}
+  }
 
-nav.navbar > .container-fluid {
+  nav.navbar > .container-fluid {
     min-height: 100px;
-}
+  }
 
-nav.navbar {
+  nav.navbar {
     min-height: 100px;
     background-color: rgba(5, 26, 46, 0.9);
-}
+  }
 
-div.title-container{
+  div.title-container {
     margin-top: 30%;
     color: white;
-}
+  }
 
-div.contact-container{
+  div.contact-container {
     margin-left: 25px;
-}
+  }
 
-div.title-container input {
+  div.title-container input {
     color: #2c3e50;
-}
+  }
 
-div.no_overflow{
+  div.no_overflow {
     overflow: hidden;
-}
+  }
 
-div.header{
-    background: linear-gradient(to right, rgb(9, 31, 52) , rgb(40, 92, 133));
-}
+  div.header {
+    background: linear-gradient(to right, rgb(9, 31, 52), rgb(40, 92, 133));
+  }
 
-div.circle_container {
+  div.circle_container {
     height: 0;
-}
-div.circle_container div.circle{
+  }
+
+  div.circle_container div.circle {
     border-radius: 50%;
     width: 10000px;
     height: 10000px;
     background: white;
-    
+
     margin-left: -4000px;
     margin-top: 35%;
-}
+  }
 
-img.logo2 {
-  width: 200px;
-  margin-right: 70px;
-  margin-top: -10px;
-}
+  img.logo2 {
+    width: 200px;
+    margin-right: 70px;
+    margin-top: -10px;
+  }
 
-img.logo {
-  margin-top: 10px;
-  margin-left: 20px;
-}
+  img.logo {
+    margin-top: 10px;
+    margin-left: 20px;
+  }
 
-img.headerimage{
+  img.headerimage {
     width: 100%;
     margin-top: 20%;
-}
+  }
 
-.section-title{
-    margin-top:10px;
+  .section-title {
+    margin-top: 10px;
     margin-bottom: 20px;
-}
+  }
 
-div.table-container {
-    margin-top:20px;
-}
+  div.table-container {
+    margin-top: 20px;
+  }
 
 
-@media only screen and (max-width: 992px) {
+  @media only screen and (max-width: 992px) {
 
-    div.title-container{
-        margin-top: 30px;
+    div.title-container {
+      margin-top: 30px;
     }
 
     div.circle_container {
-        height: 150px;
+      height: 150px;
     }
 
-    div.circle_container div.circle{
-        margin-top: 0%;
+    div.circle_container div.circle {
+      margin-top: 0%;
     }
-}
+  }
 
-@media only screen and (max-width: 770px){
+  @media only screen and (max-width: 770px) {
     img.logo2 {
-        visibility: hidden;
+      visibility: hidden;
     }
-}
+  }
 
 </style>
--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,84 +1,85 @@
 <template>
-<div id="events-container">
-  <div class="section-title">
-        <h3>Events</h3>
-        <p>To fetch new events, reload the page.</p>
-        <div id="events-standin" v-if="!events">
-            <p>(Waiting for MetaMask to retrieve past events...)</p>
-        </div>
-  </div>
-  <div class="table-container table-responsive">
-    <table id="events" v-if="!!events" class="table table-striped">
+  <div id="events-container">
+    <div class="section-title">
+      <h3>Events</h3>
+      <p>To fetch new events, reload the page.</p>
+      <div id="events-standin" v-if="!events">
+        <p>(Retrieving past events...)</p>
+      </div>
+    </div>
+    <div class="table-container table-responsive">
+      <table id="events" v-if="!!events" class="table table-striped">
         <thead>
-            <tr>
-                <th>Name</th><th colspan="100">Arguments</th>
-            </tr>
+        <tr>
+          <th>Name</th>
+          <th colspan="100">Arguments</th>
+        </tr>
         </thead>
         <tbody>
         <tr v-for="event in events" :key="JSON.stringify(event)">
-        <td>
+          <td>
             <b>
-            {{ event.event }}
+              {{ event.event }}
             </b>
-        </td>
-        <td v-for="(value, key) in event.returnValues" :key="key" v-if="key.length>1">
-        <strong>{{ key }}</strong>:<br/>
-        {{ value }}
-        <span class="value-date" v-if="key==='day'">({{ (new Date(value*1.0e3)).toLocaleString() }})</span>
-        <span class="value-numeric" v-if="key==='price'">({{ (value*1.0e-3).toFixed(3) }} ct/kWh)</span>
-        <span class="value-numeric" v-if="key==='energy'">({{ (value*1.0e-6).toFixed(6) }} kWh)</span>
-        <span class="value-date" v-if="key==='timestamp'">({{ (new Date(value*1.0e-3)).toLocaleString() }})</span>
-      </td>
-      </tr>
-      </tbody>
-    </table>
+          </td>
+          <td v-for="(value, key) in event.returnValues" :key="key" v-if="key.length>1">
+            <strong>{{ key }}</strong>:<br/>
+            {{ value }}
+            <span class="value-date" v-if="key==='day'">({{ (new Date(value*1.0e3)).toLocaleString() }})</span>
+            <span class="value-numeric" v-if="key==='price'">({{ (value*1.0e-3).toFixed(3) }} ct/kWh)</span>
+            <span class="value-numeric" v-if="key==='energy'">({{ (value*1.0e-6).toFixed(6) }} kWh)</span>
+            <span class="value-date" v-if="key==='timestamp'">({{ (new Date(value*1.0e-3)).toLocaleString() }})</span>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import sharedState from '../state.js'
-  
-export default {
+  import sharedState from '../state.js'
+
+  export default {
     name: 'Events',
     data() {
-        return sharedState
+      return sharedState
     },
     watch: {
-        contract: handleNewContract
+      contract: handleNewContract
     }
-}
+  }
 
-function handleNewContract(contract) {
+  function handleNewContract(contract) {
     // stop display old events
     this.events = null;
-    
+
     // past events
     contract.getPastEvents(
-        'allEvents', {
-            fromBlock: 0,
-            toBlock: 'latest'
-        }
+      'allEvents', {
+        fromBlock: 0,
+        toBlock: 'latest'
+      }
     ).then(events => {
-        this.events = events.reverse();
+      this.events = events.reverse();
     }).catch(error => {
-        console.log('Error from contract.getPastEvents:', error);
+      console.log('Error from contract.getPastEvents:', error);
     });
-}
+  }
 </script>
 
 <style scoped>
 
-.value-date, .value-numeric {
+  .value-date, .value-numeric {
     font-family: sans;
-}
+  }
 
-thead th{
-    text-align:center;
-}
+  thead th {
+    text-align: center;
+  }
 
-.section-title{
-    margin-top:30px;
+  .section-title {
+    margin-top: 30px;
     margin-bottom: 20px;
-}
+  }
 </style>
